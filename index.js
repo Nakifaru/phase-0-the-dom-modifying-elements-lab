5e1cfcbf3225bc5db68c98fd7324b3c55ac905c5
Michael <michaelmwendwamumo@gmail.com>
2023-04-16 16:50:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.textContent = "Arsenal is the champion."
+
+document.body.appendChild(newHeader);
+
